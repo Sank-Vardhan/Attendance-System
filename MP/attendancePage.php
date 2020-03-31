71b8c85bd0d17e53ef997a741423ecf485971cb6
Sank-Vardhan <sankalpvardhan135@gmail.com>
2020-03-31 14:02:28 +0530

    Second Commit

--- MP/attendancePage.php
+++ MP/attendancePage.php
@@ -1,48 +1,60 @@
 <?php
     session_start();
 
-
-if(!isset($_SESSION['loginId']))
-{
-	header('Location:./index.php');
-}
-echo "Welcome ".$_SESSION['loginId'] .$_SESSION['loginId1'];
-
-
+    if(!isset($_SESSION['loginId']))
+    {
+        header('Location:./index.php');
+    }
+    echo "Welcome ".$_SESSION['loginId'] .$_SESSION['loginId1'];
 ?>
-
-
-
-<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title>LogedIn</title>
     </head>
-<body>
-<h1> You have sucessfully logedIn.</h1>
-<p>Click the button to mark your attendance.</p>
-
-<button type="submit" class="btn btn-primary" name="btnLogin" onclick="getLocation()">Mark Attendance</button>
-
-<p id="demo"></p>
-
-<script>
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+    
+    <body>
+        <div>
+            <center>
+            <h1> You have sucessfully logedIn.</h1>
+            <p>Click the button to mark your attendance.</p>
+            <button type="submit" class="btn btn-primary" name="btnLogin"onclick="getLocation()">Mark Attendance</button>
+            <p id="demo"></p>
+        </center>
+        </div>
+        <script>
+            function getLocation(){
+                
+                var x = document.getElementById("demo");
+                
+                function getCoordinates(position) {
+                    const latitude  = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    
+                    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+        }
+                
+        function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    x.innerHTML = "User denied the request for Geolocation."
+                break;
+                case error.POSITION_UNAVAILABLE:
+                    x.innerHTML = "Location information is unavailable."
+                break;
+                case error.TIMEOUT:
+                    x.innerHTML = "The request to get user location timed out."
+                break;
+                case error.UNKNOWN_ERROR:
+                    x.innerHTML = "An unknown error occurred."
+                break;
+      }
+    }
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(getCoordinates,showError);
+        }
 }
-
-function showPosition(position) {
-  x.innerHTML = "Your Location is<br>""Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
-</script>
-<a href="./logout.php">Logout</a>
-</body>
+        </script>
+        <a href="./logout.php">Logout</a>
+    </body>
 </html>
